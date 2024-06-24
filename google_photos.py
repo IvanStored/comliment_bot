e574fdd500d7077c2ad48cf6bc0661d3fe6315a5
Ivan Antonishak <jus1stored@gmail.com>
2024-06-24 13:04:19 +0300

    update requirements

---
+++ google_photos.py
@@ -0,0 +1,15 @@
+import random
+
+from gphotospy import authorize
+from gphotospy.media import Media
+
+CLIENT_SECRET_FILE = "client_secret.json"
+
+service = authorize.init(CLIENT_SECRET_FILE)
+
+
+def get_random_image_url() -> str:
+    media_manager = Media(service)
+    media_iterator = media_manager.list()
+    photos = [image["baseUrl"] for image in media_iterator]
+    return random.choice(photos)
