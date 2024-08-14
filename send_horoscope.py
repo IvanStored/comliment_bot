ef51d8a60cdde9bed71d1decda837487ebea0eef
IvanStored <jus1stored@gmail.com>
2024-08-15 00:21:56 +0300

    updates

---
+++ send_horoscope.py
@@ -0,0 +1,21 @@
+from os import getenv
+
+import requests
+from dotenv import load_dotenv
+
+from horoscope import translate_horoscope_data, get_horoscope_for_today
+
+load_dotenv()
+TOKEN = getenv("TOKEN")
+ADMIN_USER_ID = int(getenv("ADMIN_USER_ID"))
+RECEIVER_USER_ID = int(getenv("RECEIVER_USER_ID"))
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+
+def send_message() -> None:
+    horoscope = translate_horoscope_data(english_data=get_horoscope_for_today())
+    requests.post(url=url, data={"chat_id": ADMIN_USER_ID, "text": horoscope})
+
+
+if __name__ == '__main__':
+    send_message()
